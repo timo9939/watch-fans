15c7c6e75a19c4c5c75c40c4858f45bbaf2da846
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-06 20:41:57 +0000

    .env file created and page is now connecting

---
+++ client/src/component/Header/index.js
@@ -0,0 +1,17 @@
+import React from "./React";
+import "./header.css";
+
+const Header =() =>{
+
+    return(
+
+        <header className="header">
+
+        </header>
+
+    )
+
+}
+
+
+export default Header;
