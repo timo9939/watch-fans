feaa1bfa669d462ae7cda0ef34daca0677bf31ae
timo9939 <timothylau20199@gmail.com>
2023-03-14 21:25:22 +0000

    Adding description and price in the category

--- client/src/component/Content/Catagories/index.js
+++ client/src/component/Content/Catagories/index.js
@@ -12,19 +12,24 @@ const Catagories = () => {
             <figure className="figure1">
             <img src={img01} className="thumbnail"></img>
             <figcaption>Rolex Watch</figcaption>
+            <p figure1>Description: Second Hand Luxury Rolex Watch</p>
+            <p figure1>Price:£ 1500</p>
             
             </figure>
 
-            <figure className="figure2">
+            <figure className="figure1">
             <img src={img02} className="thumbnail"></img>
             <figcaption>Lady watch</figcaption>
+            <p className="figure1">Description:Fashionable Lady Watch</p>
+            <p className="figure1">Price £200</p>
             
             </figure>
 
-            <figure className="figure3">
+            <figure className="figure1">
             <img src={img03} className="thumbnail"></img>
             <figcaption>Male watch</figcaption>
-            
+            <p figure1>Description: Multifunctional male watch that is worth to buy it</p>
+            <p figure1>Price:£ 800</p>
             </figure>
 
         </section>
